5ff114fc3053d550e8b3c5a64d35a213c0336c30
Harsh V Srivastava <harshvsri@gmail.com>
2025-07-15 12:27:19 +0530

    Change the server architecture to channel based

--- src/server.rs
+++ src/server.rs
@@ -1,11 +1,10 @@
-use crossterm::style::{style, Color, Stylize};
 use smol::{
+    channel::{Receiver, Sender},
     io::{AsyncReadExt, AsyncWriteExt},
-    lock::Mutex,
     net::{SocketAddr, TcpListener, TcpStream},
     stream::StreamExt,
 };
-use std::{error::Error, fmt::Display, sync::Arc};
+use std::{error::Error, fmt::Display};
 
 const ADDRESS: &str = "127.0.0.1:3000";
 const SAFE_MODE: bool = false;
@@ -26,25 +25,6 @@ where
     }
 }
 
-enum Msg {
-    ClientConnected,
-    ClientDisconnected,
-    Message(Vec<u8>),
-}
-
-impl Display for Msg {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Msg::ClientConnected => "Client Connected".fmt(f),
-            Msg::ClientDisconnected => "Client Disonnected".fmt(f),
-            Msg::Message(msg) => {
-                let message = String::from_utf8_lossy(msg);
-                format!("New Message: {:?}", message).fmt(f)
-            }
-        }
-    }
-}
-
 #[derive(Debug, Clone)]
 struct Client {
     addr: SocketAddr,
@@ -56,30 +36,7 @@ impl Client {
         Client { addr, stream }
     }
 
-    async fn broadcast(&self, conn_clients: Arc<Mutex<Vec<Client>>>, msg: &[u8]) {
-        let mut clients = conn_clients.lock().await;
-
-        for client in clients.iter_mut().filter(|client| client.addr != self.addr) {
-            if let Err(err) = client.stream.write(msg).await {
-                eprintln!(
-                    "ERROR: could not write to client {}: {}",
-                    client.addr,
-                    Sensitive(err)
-                );
-            }
-        }
-    }
-
-    async fn handle_connection(
-        &mut self,
-        conn_clients: Arc<Mutex<Vec<Client>>>,
-    ) -> Result<(), Box<dyn Error>> {
-        let conn_msg = format!("{} @ {}", Msg::ClientConnected, self.addr);
-        println!("{}", style(&conn_msg).with(Color::Green));
-
-        self.broadcast(conn_clients.clone(), conn_msg.as_bytes())
-            .await;
-
+    async fn handler(&mut self, sender: Sender<ServerAction>) -> Result<(), Box<dyn Error>> {
         let mut buf = vec![0; 1024];
 
         loop {
@@ -89,31 +46,76 @@ impl Client {
                     Sensitive(err)
                 )
             })?;
-
             match bytes_read {
                 0 => {
-                    let disconn_msg = format!("{} @ {}", Msg::ClientDisconnected, self.addr);
-                    println!("{}", style(&disconn_msg).with(Color::Red));
-
-                    self.broadcast(conn_clients.clone(), disconn_msg.as_bytes())
-                        .await;
-
-                    // Remove this client.
-                    conn_clients
-                        .lock()
-                        .await
-                        .retain(|client| client.addr != self.addr);
+                    sender
+                        .send(ServerAction::ClientDisconnected { addr: self.addr })
+                        .await?;
                     break;
                 }
                 _ => {
                     let msg = buf[..bytes_read].to_vec();
-                    println!("{}", Msg::Message(msg.clone()));
-
-                    self.broadcast(conn_clients.clone(), &msg).await;
+                    sender
+                        .send(ServerAction::Message {
+                            addr: self.addr,
+                            msg,
+                        })
+                        .await?;
                 }
             }
         }
         Ok(())
+    }
+}
+
+enum ServerAction {
+    ClientConnected { client: Client },
+    ClientDisconnected { addr: SocketAddr },
+    Message { addr: SocketAddr, msg: Vec<u8> },
+}
+
+async fn broadcast_handler(reciever: Receiver<ServerAction>) {
+    let mut clients: Vec<Client> = Vec::new();
+
+    loop {
+        if let Ok(action) = reciever.recv().await {
+            match action {
+                ServerAction::ClientConnected { client } => {
+                    let connected_msg = format!("{} has joined the chat", client.addr);
+                    println!("{connected_msg}");
+
+                    for client in clients.iter_mut() {
+                        if let Err(e) = client.stream.write_all(connected_msg.as_bytes()).await {
+                            eprintln!("Failed to write to {}: {}", client.addr, e);
+                        }
+                    }
+
+                    clients.push(client);
+                }
+                ServerAction::ClientDisconnected { addr } => {
+                    clients.retain(|client| client.addr != addr);
+
+                    let disconnected_msg = format!("{} has left the chat", addr);
+                    println!("{disconnected_msg}");
+
+                    for client in clients.iter_mut() {
+                        if let Err(e) = client.stream.write_all(disconnected_msg.as_bytes()).await {
+                            eprintln!("Failed to write to {}: {}", client.addr, e);
+                        }
+                    }
+                }
+                ServerAction::Message { addr, msg } => {
+                    let full_msg = format!("{}: {}", addr, String::from_utf8_lossy(&msg));
+                    println!("{full_msg}");
+
+                    for client in clients.iter_mut().filter(|client| client.addr != addr) {
+                        if let Err(e) = client.stream.write_all(full_msg.as_bytes()).await {
+                            eprintln!("Failed to write to {}: {}", client.addr, e);
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
@@ -124,18 +126,25 @@ fn main() -> Result<(), Box<dyn Error>> {
             .inspect_err(|err| eprintln!("ERROR: could not bind {ADDRESS}: {}", Sensitive(err)))?;
         println!("SERVER: listening at {addr}", addr = Sensitive(ADDRESS));
 
-        let clients = Arc::new(Mutex::new(Vec::new()));
+        let (sender, reciever) = smol::channel::unbounded::<ServerAction>();
+
+        smol::spawn(async move { broadcast_handler(reciever).await }).detach();
 
         while let Some(stream) = listener.incoming().next().await {
             match stream {
                 Ok(stream) => match stream.peer_addr() {
                     Ok(addr) => {
                         let mut client = Client::new(addr, stream);
-                        clients.lock().await.push(client.clone());
 
-                        let conn_clients = clients.clone();
+                        sender
+                            .send(ServerAction::ClientConnected {
+                                client: client.clone(),
+                            })
+                            .await?;
+
+                        let sender = sender.clone();
                         smol::spawn(async move {
-                            if let Err(err) = client.handle_connection(conn_clients).await {
+                            if let Err(err) = client.handler(sender).await {
                                 eprintln!(
                                     "ERROR: client {} disconnected with an error: {}",
                                     client.addr,
